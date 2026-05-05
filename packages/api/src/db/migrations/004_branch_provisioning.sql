51d1e5cff65597cf57760c8e3f6ea41e38a52f13
Ignatious Chihwayi <chihwayii@outlook.com>
2026-05-05 14:22:47 +0200

    refactor(migrations): remove IF NOT EXISTS from column addition statements

--- packages/api/src/db/migrations/004_branch_provisioning.sql
+++ packages/api/src/db/migrations/004_branch_provisioning.sql
@@ -1,4 +1,4 @@
 -- Add provisioning columns to nx_branches
 ALTER TABLE nx_branches
-  ADD COLUMN IF NOT EXISTS radius_secret VARCHAR(64) NOT NULL DEFAULT '',
-  ADD COLUMN IF NOT EXISTS reg_token     VARCHAR(64) NULL DEFAULT NULL;
+  ADD COLUMN radius_secret VARCHAR(64) NOT NULL DEFAULT '',
+  ADD COLUMN reg_token     VARCHAR(64) NULL DEFAULT NULL;
